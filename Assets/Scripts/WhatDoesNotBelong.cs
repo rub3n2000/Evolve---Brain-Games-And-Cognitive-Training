d63c8a3fec2fd8e4be0277f2d2a5844e365b56f4
rub3n2000 <37822856+rub3n2000@users.noreply.github.com>
2019-04-07 08:16:16 +0200

    setup framework of whatdoesnotbelong game

---
+++ Assets/Scripts/WhatDoesNotBelong.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhatDoesNotBelong : MonoBehaviour
+{
+    int maxRounds = 10;
+    int currentRound = 0;
+    SessionManager sessionManager;
+    SaveLoader saveLoader;
+    ScoreKeeper scoreKeeper;
+    [SerializeField]
+    Text button1Text;
+    [SerializeField]
+    Text button2Text;
+    [SerializeField]
+    Text button3Text;
+    [SerializeField]
+    Text button4Text;
+    Color originalColor;
+    int currentAnswerId;
+    [SerializeField]
+    GameObject game;
+    [SerializeField]
+    GameObject endScreen;
+    [SerializeField]
+    Text endscreenText;
+
+    List<int> scores;
+    bool canAnswer = true;
+    [SerializeField]
+    Riddle[] riddles;
+
+    List<string> correctAnswers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sessionManager = FindObjectOfType<SessionManager>();
+        saveLoader = FindObjectOfType<SaveLoader>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        endScreen.SetActive(false);
+        game.SetActive(true);
+        originalColor = button1Text.color;
+        scores = new List<int>();
+        correctAnswers = new List<string>();
+        SetupRound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void Guess(int id)
+    {
+        if (canAnswer)
+        {
+            canAnswer = false;
+            switch (currentAnswerId)
+            {
+                case 0:
+                    button1Text.color = Color.green; button2Text.color = Color.red; button3Text.color = Color.red; button4Text.color = Color.red;
+                    break;
+                case 1:
+                    button1Text.color = Color.red; button2Text.color = Color.green; button3Text.color = Color.red; button4Text.color = Color.red;
+                    break;
+                case 2:
+                    button1Text.color = Color.red; button2Text.color = Color.red; button3Text.color = Color.green; button4Text.color = Color.red;
+                    break;
+                case 3:
+                    button1Text.color = Color.red; button2Text.color = Color.red; button3Text.color = Color.red; button4Text.color = Color.green;
+                    break;
+            }
+
+            if (id == currentAnswerId)
+            {
+                scoreKeeper.languagePoints += 100;
+                scores.Add(100);
+                if (scoreKeeper.languagePoints > scoreKeeper.pointsRequiredForLevel[scoreKeeper.languageLevel + 1])
+                {
+                    scoreKeeper.languageLevel++;
+                }
+                saveLoader.SaveGameData();
+            }
+            else { scores.Add(0); }
+            Invoke("StartNewRound", 1);
+        }
+    }
+
+    public void ContinueSession()
+    {
+        sessionManager.ContinueSession();
+    }
+
+    void EndGame()
+    {
+        endScreen.SetActive(true);
+        game.SetActive(false);
+        endscreenText.text = "";
+        for (int i = 0; i < scores.Count; i++)
+        {
+            if (scores[i] == 0)
+            {
+                endscreenText.text += "Round " + i + " | Wrong |" + "\n";
+            }
+            else
+            {
+                endscreenText.text += "Round " + i + " | Correct | " + scores[i] + " points" + "\n";
+            }
+        }
+        endscreenText.text += "\n" + " Well Done, Keep Improving!";
+    }
+
+    void StartNewRound()
+    {
+        if (currentRound >= 10)
+        {
+            EndGame();
+        }
+        else
+        {
+            button1Text.color = originalColor; button2Text.color = originalColor;
+            button3Text.color = originalColor; button4Text.color = originalColor;
+            currentRound++;
+            canAnswer = true;
+            SetupRound();
+        }
+    }
+
+    void SetupRound()
+    {
+        int riddleIndex = Random.Range(0, riddles.Length);
+        currentAnswerId = Random.Range(0, 4);
+        int firstWrongOne = Random.Range(0, 4);
+        while(firstWrongOne == currentAnswerId) { firstWrongOne = Random.Range(0, 4); }
+        int secondWrongOne = Random.Range(0, 4);
+        while (secondWrongOne == currentAnswerId || secondWrongOne == firstWrongOne) { secondWrongOne = Random.Range(0, 4); }
+        int thirdWrongOne = Random.Range(0, 4);
+        while (thirdWrongOne == currentAnswerId || thirdWrongOne == firstWrongOne || thirdWrongOne == secondWrongOne) { thirdWrongOne = Random.Range(0, 4); }
+        correctAnswers.Add(riddles[riddleIndex].theOneThatDoesntBelong);
+        switch(currentAnswerId)
+        {
+            case 0: button1Text.text = riddles[riddleIndex].theOneThatDoesntBelong; break;
+            case 1: button2Text.text = riddles[riddleIndex].theOneThatDoesntBelong; break;
+            case 2: button3Text.text = riddles[riddleIndex].theOneThatDoesntBelong; break;
+            case 3: button4Text.text = riddles[riddleIndex].theOneThatDoesntBelong; break;
+        }
+        switch (firstWrongOne)
+        {
+            case 0: button1Text.text = riddles[riddleIndex].firstThatBelongs; break;
+            case 1: button2Text.text = riddles[riddleIndex].firstThatBelongs; break;
+            case 2: button3Text.text = riddles[riddleIndex].firstThatBelongs; break;
+            case 3: button4Text.text = riddles[riddleIndex].firstThatBelongs; break;
+        }
+        switch (secondWrongOne)
+        {
+            case 0: button1Text.text = riddles[riddleIndex].secondThatBelongs; break;
+            case 1: button2Text.text = riddles[riddleIndex].secondThatBelongs; break;
+            case 2: button3Text.text = riddles[riddleIndex].secondThatBelongs; break;
+            case 3: button4Text.text = riddles[riddleIndex].secondThatBelongs; break;
+        }
+        switch (thirdWrongOne)
+        {
+            case 0: button1Text.text = riddles[riddleIndex].thirdThatBelongs; break;
+            case 1: button2Text.text = riddles[riddleIndex].thirdThatBelongs; break;
+            case 2: button3Text.text = riddles[riddleIndex].thirdThatBelongs; break;
+            case 3: button4Text.text = riddles[riddleIndex].thirdThatBelongs; break;
+        }
+    }
+
+    [System.Serializable]
+    struct Riddle
+    {
+        public string theOneThatDoesntBelong;
+        public string firstThatBelongs;
+        public string secondThatBelongs;
+        public string thirdThatBelongs;
+    }
+}
